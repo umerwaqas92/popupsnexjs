2a7c575ef4c89a7123e8bc901103ec5f2fe8feb6
umerwaqas92 <vovokhan@gmail.com>
2024-06-10 14:55:01 +0500

    addded google amylaytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 // import { GoogleAnalytics } from '@next/third-parties/google'
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html data-theme="mytheme" lang="en">
 
-{/* <GoogleAnalytics gaId={"G-Z53XJ5649S"} /> */}
 
       <body className={inter.className}>
         
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
 
         {children}
         </body>
+        <GoogleAnalytics gaId={"G-Z53XJ5649S"} />
+
+
     </html>
   );
 }
